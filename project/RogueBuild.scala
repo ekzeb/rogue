1a4757d28772e934be81d3a8ce10df01df6b98dc
alex <ekzeboff@gmail.com>
2014-10-17 09:11:24 +0400

    Scala 2.11, Lift 2.6, 3.0

--- project/RogueBuild.scala
+++ project/RogueBuild.scala
@@ -4,18 +4,22 @@ import Keys._
 
 object RogueBuild extends Build {
   override lazy val projects =
-    Seq(all, index, core, lift, spindle)
+    Seq(all, index, core, lift/*, spindle*/)
 
   lazy val all: Project = Project("all", file(".")) aggregate(
-    index, core, lift, spindle)
+    index, core, lift/*, spindle*/)
 
   lazy val index = Project("rogue-index", file("rogue-index/")) dependsOn()
   lazy val core = Project("rogue-core", file("rogue-core/")) dependsOn(index % "compile;test->test;runtime->runtime")
   lazy val lift = Project("rogue-lift", file("rogue-lift/")) dependsOn(core % "compile;test->test;runtime->runtime")
-  lazy val spindle = Project("rogue-spindle", file("rogue-spindle/")) dependsOn(core % "compile;test->test;runtime->runtime")
+  //lazy val spindle = Project("rogue-spindle", file("rogue-spindle/")) dependsOn(core % "compile;test->test;runtime->runtime")
+
+  lazy val lift211Version = settingKey[String]("Scala 2.11 Lift version")
 
   lazy val defaultSettings: Seq[Setting[_]] = Seq(
     version := "3.0.0-beta7a",
+    scalaVersion := "2.11.2",
+    lift211Version := "3.0-M2",
     organization := "com.foursquare",
     crossScalaVersions := Seq("2.9.1", "2.9.2", "2.10.2"),
     publishMavenStyle := true,
@@ -50,8 +54,9 @@ object RogueBuild extends Build {
       </developers>),
     resolvers ++= Seq(
         "Bryan J Swift Repository" at "http://repos.bryanjswift.com/maven2/",
-        "Releases" at "http://oss.sonatype.org/content/repositories/releases",
-        "Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots"),
+        "Releases" at "https://oss.sonatype.org/content/repositories/releases",
+        "Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"),
+    resolvers += Resolver.file("localIvy", file(Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns),
     retrieveManaged := true,
     scalacOptions ++= Seq("-deprecation", "-unchecked"),
     scalacOptions <++= scalaVersion map { scalaVersion =>
